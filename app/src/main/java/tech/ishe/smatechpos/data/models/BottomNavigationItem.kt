2229ed5db487aa85939e2bb3fee5f5ae4770d1e2
Ishe <38261770+ishe19@users.noreply.github.com>
2024-11-16 12:56:29 +0200

    Update BottomNavigationItem.kt

--- app/src/main/java/tech/ishe/smatechpos/data/models/BottomNavigationItem.kt
+++ app/src/main/java/tech/ishe/smatechpos/data/models/BottomNavigationItem.kt
@@ -4,6 +4,5 @@ import androidx.compose.ui.graphics.vector.ImageVector
 
 data class BottomNavigationItem(
     val title: String,
-    val icon: ImageVector,
-    val route: String
+    val icon: ImageVector
 )

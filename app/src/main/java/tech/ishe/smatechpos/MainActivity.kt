d032b81978edb16534898eea40ffa17f489a71e1
Ishe <38261770+ishe19@users.noreply.github.com>
2024-11-16 15:32:22 +0200

    Update MainActivity.kt

--- app/src/main/java/tech/ishe/smatechpos/MainActivity.kt
+++ app/src/main/java/tech/ishe/smatechpos/MainActivity.kt
@@ -4,13 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -19,9 +17,12 @@ import com.google.gson.Gson
 import tech.ishe.smatechpos.data.models.ProductModel
 import tech.ishe.smatechpos.ui.theme.SmatechPOSTheme
 import tech.ishe.smatechpos.viewmodels.CartViewModel
+import tech.ishe.smatechpos.viewmodels.LoadingViewModel
+import tech.ishe.smatechpos.viewmodels.OrdersViewModel
 import tech.ishe.smatechpos.viewmodels.ProductsViewModel
 import tech.ishe.smatechpos.views.home.MainScreen
 import tech.ishe.smatechpos.views.home.ProductDetailsScreen
+import tech.ishe.smatechpos.views.utils.LoadingOverlay
 import tech.ishe.smatechpos.views.utils.Routes
 import tech.ishe.smatechpos.views.utils.ScreenDimensions
 
@@ -33,6 +34,8 @@ class MainActivity : ComponentActivity() {
 
         val productsViewModel = ViewModelProvider(this)[ProductsViewModel::class.java]
         val shoppingCartViewModel = ViewModelProvider(this)[CartViewModel::class.java]
+        val ordersViewModel = ViewModelProvider(this)[OrdersViewModel::class.java]
+        val loadingViewModel = ViewModelProvider(this)[LoadingViewModel::class.java]
         enableEdgeToEdge()
         setContent {
 
@@ -40,17 +43,24 @@ class MainActivity : ComponentActivity() {
             ScreenDimensions.initialize(configuration)
             SmatechPOSTheme {
                 val navController = rememberNavController()
-
                 NavHost(navController = navController, startDestination = Routes.mainScreen,
                     builder = {
-                        composable(Routes.mainScreen) { MainScreen(productsViewModel, navController) }
+                        composable(Routes.mainScreen) {
+                            MainScreen(
+                                productsViewModel,
+                                navController
+                            )
+                        }
                         composable(Routes.productDetails + "/{product}") { backStackEntry ->
                             val productJson = backStackEntry.arguments?.getString("product")
-                            val productModel = Gson().fromJson(productJson, ProductModel::class.java)
+                            val productModel =
+                                Gson().fromJson(productJson, ProductModel::class.java)
                             ProductDetailsScreen(productModel)
                         }
                     }
                 )
+
+                loadingViewModel.loading.value?.let { LoadingOverlay(isLoading = it) }
             }
         }
     }

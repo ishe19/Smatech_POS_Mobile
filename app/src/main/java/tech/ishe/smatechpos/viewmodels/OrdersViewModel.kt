3f4835526a4912e5eb458b54089f99367e8b1db2
Ishe <38261770+ishe19@users.noreply.github.com>
2024-11-16 21:49:48 +0200

    Update OrdersViewModel.kt

--- app/src/main/java/tech/ishe/smatechpos/viewmodels/OrdersViewModel.kt
+++ app/src/main/java/tech/ishe/smatechpos/viewmodels/OrdersViewModel.kt
@@ -9,13 +9,14 @@ import kotlinx.coroutines.launch
 import tech.ishe.smatechpos.data.api.RetrofitInstance
 import tech.ishe.smatechpos.data.models.GenericResponse
 import tech.ishe.smatechpos.data.models.NetworkResponse
+import tech.ishe.smatechpos.data.models.OrderModel
 import tech.ishe.smatechpos.data.models.OrderRequest
 
 class OrdersViewModel : ViewModel() {
 
     private val posApi = RetrofitInstance.posApi
-    private val _orderPostResult = MutableLiveData<NetworkResponse<GenericResponse>>()
-    val orderPostResult: LiveData<NetworkResponse<GenericResponse>> = _orderPostResult
+    private val _orderPostResult = MutableLiveData<NetworkResponse<GenericResponse>?>()
+    val orderPostResult: LiveData<NetworkResponse<GenericResponse>?> = _orderPostResult
 
     fun placeOrder(orderRequest: OrderRequest) {
         _orderPostResult.value = NetworkResponse.Loading
@@ -25,6 +26,7 @@ class OrdersViewModel : ViewModel() {
                 val response = posApi.postOrder(orderRequest)
 
                 if (response.isSuccessful) {
+                    Log.i("RESPONSE", response.body().toString())
                     response.body()?.let {
                         _orderPostResult.value = NetworkResponse.Success(it)
                     }
@@ -44,6 +46,34 @@ class OrdersViewModel : ViewModel() {
 
     fun clearResult() {
         _orderPostResult.value = null
+    }
+
+    private val _ordersResult = MutableLiveData<NetworkResponse<List<OrderModel>>>()
+    val ordersResult: LiveData<NetworkResponse<List<OrderModel>>> = _ordersResult
+
+    fun getOrders() {
+        _ordersResult.value = NetworkResponse.Loading
+
+        viewModelScope.launch {
+            try {
+                val response = posApi.getOrders()
+
+                if (response.isSuccessful) {
+                    response.body()?.let {
+                        _ordersResult.value = NetworkResponse.Success(it.data)
+                    }
+                } else {
+                    Log.e("Failure response", response.toString())
+                    _ordersResult.value =
+                        NetworkResponse.Error("Failed to load orders at the moment...")
+                }
+            } catch (e: Exception) {
+                e.message?.let { Log.e("Post Order Error", it) }
+                _ordersResult.value =
+                    NetworkResponse.Error("Failed to load orders at the moment...")
+
+            }
+        }
     }
 
 }

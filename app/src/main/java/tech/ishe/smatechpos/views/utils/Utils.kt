eb199821faa2e2265669a041d429413bcfcc0d38
Ishe <38261770+ishe19@users.noreply.github.com>
2024-11-16 12:56:38 +0200

    Update Utils.kt

--- app/src/main/java/tech/ishe/smatechpos/views/utils/Utils.kt
+++ app/src/main/java/tech/ishe/smatechpos/views/utils/Utils.kt
@@ -1,7 +1,12 @@
 package tech.ishe.smatechpos.views.utils
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.CreditCard
+import androidx.compose.material.icons.rounded.Home
+import androidx.compose.material.icons.rounded.ShoppingCart
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import tech.ishe.smatechpos.data.models.BottomNavigationItem
 
 fun getGradient(
     startColor: Color,
@@ -11,3 +16,20 @@ fun getGradient(
         colors = listOf(startColor, endColor)
     )
 }
+
+val barItems = listOf(
+    BottomNavigationItem(
+        title = "Home",
+        icon = Icons.Rounded.Home
+    ),
+
+    BottomNavigationItem(
+        title = "Cart",
+        icon = Icons.Rounded.ShoppingCart
+    ),
+
+    BottomNavigationItem(
+        title = "Receipts",
+        icon = Icons.Rounded.CreditCard
+    ),
+)

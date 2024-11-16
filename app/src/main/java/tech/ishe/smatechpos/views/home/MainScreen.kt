547f2b58f39688869b2c328eae8b8ea9e64f144a
Ishe <38261770+ishe19@users.noreply.github.com>
2024-11-16 23:09:05 +0200

    Update MainScreen.kt

--- app/src/main/java/tech/ishe/smatechpos/views/home/MainScreen.kt
+++ app/src/main/java/tech/ishe/smatechpos/views/home/MainScreen.kt
@@ -1,18 +1,23 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package tech.ishe.smatechpos.views.home
 
 import androidx.activity.ComponentActivity
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Badge
 import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
@@ -20,13 +25,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import tech.ishe.smatechpos.viewmodels.CartViewModel
 import tech.ishe.smatechpos.viewmodels.ProductsViewModel
 import tech.ishe.smatechpos.views.cart.CartTab
 import tech.ishe.smatechpos.views.receipts.ReceiptsTab
-import tech.ishe.smatechpos.views.utils.LoadingOverlay
 import tech.ishe.smatechpos.views.utils.barItems
 
 @Composable
@@ -84,6 +90,16 @@ fun MainScreen(productsViewModel: ProductsViewModel, navController: NavControlle
                     }
                 }
             }
+        },
+        topBar = {
+            TopAppBar(title = {
+                Text(
+                    text = "Smatech POS",
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.fillMaxWidth(),
+                    fontWeight = FontWeight.Bold,
+                ) }
+            )
         }
     ) { innerPadding ->
         ContentScreen(
@@ -97,15 +113,15 @@ fun MainScreen(productsViewModel: ProductsViewModel, navController: NavControlle
 
 @Composable
 fun ContentScreen(
-    modifier: Modifier = Modifier,
+    modifier: Modifier,
     selectedIndex: Int,
     productsViewModel: ProductsViewModel,
     navController: NavController
 ) {
     when (selectedIndex) {
-        0 -> HomeTab(viewModel = productsViewModel, navController = navController)
-        1 -> CartTab()
-        2 -> ReceiptsTab(navController)
+        0 -> HomeTab(productsViewModel = productsViewModel, navController = navController, modifier)
+        1 -> CartTab(modifier)
+        2 -> ReceiptsTab(navController, modifier)
     }
 }
 

91fb49e2c68b512e165513ca36fe5c9f221e315e
Ishe <38261770+ishe19@users.noreply.github.com>
2024-11-16 15:33:40 +0200

    Update ReceiptsTab.kt

--- app/src/main/java/tech/ishe/smatechpos/views/receipts/ReceiptsTab.kt
+++ app/src/main/java/tech/ishe/smatechpos/views/receipts/ReceiptsTab.kt
@@ -1,20 +1,55 @@
 package tech.ishe.smatechpos.views.receipts
 
+import androidx.activity.ComponentActivity
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModelProvider
+import tech.ishe.smatechpos.data.models.OrderModel
+import tech.ishe.smatechpos.viewmodels.OrdersViewModel
 
 @Preview(showBackground = true)
 @Composable
 fun ReceiptsTab() {
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .padding(8.dp)) {
-        Text(text = "Receipts Tab")
+    val context = LocalContext.current
+    val ordersViewModel: OrdersViewModel = ViewModelProvider(context as ComponentActivity)[OrdersViewModel::class.java]
+
+
+    var receiptsList: List<OrderModel> = emptyList()
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(vertical = 32.dp, horizontal = 4.dp),
+    ) {
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        LazyColumn (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        ){
+            if(receiptsList.isEmpty()){
+                item {
+                    Text(text = "No order receipts at the moment...")
+                }
+            } else {
+                items(receiptsList.size){ index ->
+                    ReceiptCard(receiptsList[index])
+                }
+            }
+        }
+
     }
 }

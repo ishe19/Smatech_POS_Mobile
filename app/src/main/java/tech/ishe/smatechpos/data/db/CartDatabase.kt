622f629bd654f1e586f0f16e73acf8e51861533d
Ishe <38261770+ishe19@users.noreply.github.com>
2024-11-18 16:52:49 +0200

    Update CartDatabase.kt

--- app/src/main/java/tech/ishe/smatechpos/data/db/CartDatabase.kt
+++ app/src/main/java/tech/ishe/smatechpos/data/db/CartDatabase.kt
@@ -4,7 +4,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import tech.ishe.smatechpos.data.models.CartItemModel
 
-@Database(entities = [CartItemModel::class], version = 1)
+@Database(entities = [CartItemModel::class], version = 1, exportSchema=false)
 abstract class CartDatabase: RoomDatabase() {
 
     companion object{
